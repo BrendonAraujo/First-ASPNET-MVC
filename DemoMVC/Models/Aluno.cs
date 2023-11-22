4d9a52b01d479e363a112914e194bd0a2f9c0ccc
BrendonAraujo <brendong.araujo@gmail.com>
2023-11-22 18:50:55 -0300

    #ADD - Inicial Commit

---
+++ DemoMVC/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+
+    public bool Active { get; set; }
+}
